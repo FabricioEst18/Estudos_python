0a6c570ce4fcaefbd0e4a1d974d1cac931deef77
Fabricio <fabricioestevam46@gmail.com>
2025-08-27 06:36:22 -0300

    Operadores em Python

---
+++ operadores.py
@@ -0,0 +1,27 @@
+#OPERADORES ARITMÉTICOS:
+# + → Soma
+# - → Subtração
+# * → Multiplicação
+# / → Divisão
+# % → Resto da divisão(mod)
+# ** → Potenciação
+# // → Divisão inteira
+x = 5
+y = 2
+
+z = 5 / 2
+
+
+#OPERADORES COMPARATIVOS:
+# < → Menor que
+# > → Maior que
+# <= → Menor ou igual
+# >= → Maior ou igual
+# == → Igual 
+# != ou <> → Diferente
+
+
+#OPERADORES LÓGICOS: 
+# and → e 
+# or → ou 
+# not → não
